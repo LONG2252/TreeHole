e9604d4d8a87feabf6f94d2c1f78d9da9e363567
LONG2252 <384810980@qq.com>
2017-11-13 23:02:21 +0800

    ...

---
+++ TreeHole/js/demo.js
@@ -0,0 +1,10 @@
+﻿function LoginBox()
+{
+    alert("welcome !");
+    layer.open({
+        type:1,
+        title:"登录",
+        area:["214px", "200px"],
+        content:$("#loginbox")
+    })
+}
