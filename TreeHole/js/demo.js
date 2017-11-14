de7ba52ccad871f86da1acdd161e35e215b9ef67
LONG2252 <384810980@qq.com>
2017-11-14 09:06:14 +0800

    面账户验证

--- TreeHole/js/demo.js
+++ TreeHole/js/demo.js
@@ -1,10 +1,24 @@
 ﻿function LoginBox()
 {
-    alert("welcome !");
     layer.open({
         type:1,
         title:"登录",
         area:["214px", "200px"],
         content:$("#loginbox")
     })
+}
+
+function Login() {
+    var username = $.trim($("#txtUserName").val()); //获取用户名
+    var pwd = $.trim($("#txtPwd").val()); //获取密码
+    if (username == "" || pwd == "") {
+        layer.alert("用户名或密码不能为空！",
+            {
+                title: '提示',
+                icon:5
+            })
+    }
+    else {
+        $.post("后台页面",{"username":username,"pwd":pwd}, function(data){})   /*后台验证*/
+    }
 }
